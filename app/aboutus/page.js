12673ed12a3a645e191cda20876787670de084a9
Eranda Sanjaya <76166067+ES-3508@users.noreply.github.com>
2024-09-23 21:57:37 +0530

    Fix: Routine and nav bar

--- app/aboutus/page.js
+++ app/aboutus/page.js
@@ -1,6 +1,6 @@
 import React from "react";
-import TeamSplaher from "../../../components/team/TeamSplasher";
-import TeamMember from "../../../components/team/TeamMember";
+import TeamSplaher from "../../components/team/TeamSplasher";
+import TeamMember from "../../components/team/TeamMember";
 
 export default function Team_pg() {
   return (
