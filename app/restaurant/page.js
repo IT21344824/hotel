cb9358ac742789f1112f4dbcfdc01ff01a9c8c9b
Eranda Sanjaya <76166067+ES-3508@users.noreply.github.com>
2024-09-23 22:03:06 +0530

    Fix: routine

--- app/restaurant/page.js
+++ app/restaurant/page.js
@@ -1,6 +1,6 @@
 import React from "react";
-import MenuSplaher from "../../../components/menu/MenuSplasher";
-import MenuContainer from "../../../components/menu/MenuContainer";
+import MenuSplaher from "../../components/menu/MenuSplasher";
+import MenuContainer from "../../components/menu/MenuContainer";
 
 const page = () => {
   return (
