6a96b40ee77f79afa727b0d968aebbb4aab4a68d
Akidu Ekanayake <IT21344824@my.sliit.lk>
2024-09-25 20:23:16 +0530

    event all category ui has added

--- app/events/category/page.js
+++ app/events/category/page.js
@@ -4,6 +4,24 @@ import CategoryContainer from "@/components/eventCategory/CategoryContainer";
 import {
   eventCategorDummy_1,
   eventCategorDummy_2,
+  eventCategorDummy_3,
+  eventCategorDummy_4,
+  eventCategorDummy_5,
+  eventCategorDummy_6,
+  eventCategorDummy_7,
+  eventCategorDummy_8,
+  eventCategorDummy_9,
+  eventCategorDummy_10,
+  eventCategorDummy_11,
+  eventCategorDummy_12,
+  eventCategorDummy_13,
+  eventCategorDummy_14,
+  eventCategorDummy_15,
+  eventCategorDummy_16,
+  eventCategorDummy_17,
+  eventCategorDummy_18,
+  eventCategorDummy_19,
+  eventCategorDummy_20,
 } from "@/components/dummy/eventCategorDummy";
 
 const eventCategoryPG = () => {
@@ -14,20 +32,85 @@ const eventCategoryPG = () => {
       sub_heading: "Drinks Options",
       data: eventCategorDummy_2,
     },
-  ]; // Add more categories as needed
+    { title: "Music", sub_heading: "", data: eventCategorDummy_3 },
+    {
+      title: "Meals - Main Course",
+      sub_heading: "Meat Options",
+      data: eventCategorDummy_4,
+    },
+    {
+      title: "Meals - Main Course ",
+      sub_heading: "Seafood Options",
+      data: eventCategorDummy_5,
+    },
+    {
+      title: "Meals - Main Course",
+      sub_heading: "Vegetarian/Vegan Options",
+      data: eventCategorDummy_6,
+    },
+    {
+      title: "Meals",
+      sub_heading: "Cold Appetizers",
+      data: eventCategorDummy_7,
+    },
+    {
+      title: "Meals",
+      sub_heading: "Hot Appetizers",
+      data: eventCategorDummy_8,
+    },
+    { title: "Meals", sub_heading: "Salad", data: eventCategorDummy_9 },
+    {
+      title: "Desserts",
+      sub_heading: "Dessert Table",
+      data: eventCategorDummy_10,
+    },
+    {
+      title: "Desserts",
+      sub_heading: "Specialty Desserts",
+      data: eventCategorDummy_11,
+    },
+    {
+      title: "Beverages",
+      sub_heading: "Non-Alcoholic",
+      data: eventCategorDummy_12,
+    },
+    {
+      title: "Beverages",
+      sub_heading: "Alcoholic ",
+      data: eventCategorDummy_13,
+    },
+    { title: "Wedding flowers ", sub_heading: "", data: eventCategorDummy_14 },
+    { title: "Photography", sub_heading: "", data: eventCategorDummy_15 },
+    {
+      title: "Wedding Invitations",
+      sub_heading: "",
+      data: eventCategorDummy_16,
+    },
+    {
+      title: "Wedding Gifts (For Guests)",
+      sub_heading: "",
+      data: eventCategorDummy_17,
+    },
+    { title: "Wedding Halls", sub_heading: "", data: eventCategorDummy_18 },
+    { title: "Bridal wears", sub_heading: "", data: eventCategorDummy_19 },
+    { title: "Groom wears", sub_heading: "", data: eventCategorDummy_20 },
+  ];
 
   return (
-    <div className="mb-20">
+    <div className="mb-20 w-full">
       <CategorySplasher />
-      {/* Loop through event categories and render CategoryContainer for each */}
-      {eventCategories.map((category, index) => (
-        <CategoryContainer
-          key={index}
-          category={category.data}
-          title={category.title}
-          sub_heading={category.sub_heading}
-        />
-      ))}
+
+      {/*here it's Loop through event categories and render CategoryContainer for each */}
+      <div className="flex w-full flex-col items-center justify-center">
+        {eventCategories.map((category, index) => (
+          <CategoryContainer
+            key={index}
+            category={category.data}
+            title={category.title}
+            sub_heading={category.sub_heading}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- components/dummy/eventCategorDummy.jsx
+++ components/dummy/eventCategorDummy.jsx
@@ -135,3 +135,423 @@ export const eventCategorDummy_2 = [
     },
 
 ];
+
+export const eventCategorDummy_3 = [
+    {
+        id: "1",
+        name: "Name",
+        img: "/images/events/category/music-1.jpeg",
+
+    },
+    {
+        id: "2",
+        name: "Name",
+        img: "/images/events/category/music-2.jpeg",
+
+    },
+    {
+        id: "3",
+        name: "Name",
+        img: "/images/events/category/music-3.jpeg",
+
+    },
+    {
+        id: "4",
+        name: "Name",
+        img: "/images/events/category/music-4.jpeg",
+    },
+
+
+
+];
+
+export const eventCategorDummy_4 = [
+    {
+        id: "1",
+        name: "Meat Options 1",
+        img: "/images/events/category/Meat Options-1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Meat Options 2",
+        img: "/images/events/category/Meat Options-2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Meat Options 3",
+        img: "/images/events/category/Meat Options-3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Meat Options 4",
+        img: "/images/events/category/Meat Options-4.jpeg",
+    },
+];
+
+export const eventCategorDummy_5 = [
+    {
+        id: "1",
+        name: "Seafood Options 1",
+        img: "/images/events/category/Seafood Options-1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Seafood Options 2",
+        img: "/images/events/category/Seafood Options-2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Seafood Options 3",
+        img: "/images/events/category/Seafood Options-3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Seafood Options 4",
+        img: "/images/events/category/Seafood Options-4.jpeg",
+    },
+];
+
+export const eventCategorDummy_6 = [
+    {
+        id: "1",
+        name: "Vegetarian/Vegan Options 1",
+        img: "/images/events/category/Vegetarian Options-1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Vegetarian/Vegan Options 2",
+        img: "/images/events/category/Vegetarian Options-2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Vegetarian/Vegan Options 3",
+        img: "/images/events/category/Vegetarian Options-3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Vegetarian/Vegan Options 4",
+        img: "/images/events/category/Vegetarian Options-4.jpeg",
+    },
+];
+
+export const eventCategorDummy_7 = [
+    {
+        id: "1",
+        name: "Cold Appetizers",
+        img: "/images/events/category/Cold Appetizers 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Cold Appetizers",
+        img: "/images/events/category/Cold Appetizers 2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Cold Appetizers",
+        img: "/images/events/category/Cold Appetizers 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Cold Appetizers",
+        img: "/images/events/category/Cold Appetizers 4.jpeg",
+    },
+];
+
+export const eventCategorDummy_8 = [
+    {
+        id: "1",
+        name: "Hot Appetizers",
+        img: "/images/events/category/Hot Appetizers 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Hot Appetizers",
+        img: "/images/events/category/Hot Appetizers 2 3.jpeg",
+    },
+    {
+        id: "3",
+        name: "Hot Appetizers",
+        img: "/images/events/category/Hot Appetizers 2 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Hot Appetizers",
+        img: "/images/events/category/Hot Appetizers 4.jpeg",
+    },
+];
+
+export const eventCategorDummy_9 = [
+    {
+        id: "1",
+        name: "Salad",
+        img: "/images/events/category/Salad 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Salad",
+        img: "/images/events/category/Salad 2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Salad",
+        img: "/images/events/category/Salad 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Salad",
+        img: "/images/events/category/Salad 4.jpeg",
+    },
+];
+
+export const eventCategorDummy_10 = [
+    {
+        id: "1",
+        name: "Dessert Table",
+        img: "/images/events/category/Dessert Table 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Dessert Table",
+        img: "/images/events/category/Dessert Table 2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Dessert Table",
+        img: "/images/events/category/Dessert Table 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Dessert Table",
+        img: "/images/events/category/Dessert Table 4.jpeg",
+    },
+];
+
+export const eventCategorDummy_11 = [
+    {
+        id: "1",
+        name: "Specialty Desserts",
+        img: "/images/events/category/Specialty Desserts 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Specialty Desserts",
+        img: "/images/events/category/Specialty Desserts 2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Specialty Desserts",
+        img: "/images/events/category/Specialty Desserts 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Specialty Desserts",
+        img: "/images/events/category/Specialty Desserts 4.jpeg",
+    },
+];
+
+export const eventCategorDummy_12 = [
+    {
+        id: "1",
+        name: "Non-Alcoholic Drink",
+        img: "/images/events/category/Non-Alcoholic 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Non-Alcoholic Drink",
+        img: "/images/events/category/Non-Alcoholic 2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Non-Alcoholic Drink",
+        img: "/images/events/category/Non-Alcoholic 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Non-Alcoholic Drink",
+        img: "/images/events/category/Non-Alcoholic 4.jpeg",
+    },
+];
+
+export const eventCategorDummy_13 = [
+    {
+        id: "1",
+        name: "Alcoholic Drink",
+        img: "/images/events/category/Alcoholic 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Alcoholic Drink",
+        img: "/images/events/category/Alcoholic 2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Alcoholic Drink",
+        img: "/images/events/category/Alcoholic 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Alcoholic Drink",
+        img: "/images/events/category/Alcoholic 4.jpeg",
+    },
+];
+
+export const eventCategorDummy_14 = [
+    {
+        id: "1",
+        name: "Wedding Flowers",
+        img: "/images/events/category/Wedding flowers 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Wedding Flowers",
+        img: "/images/events/category/Wedding flowers 2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Wedding Flowers",
+        img: "/images/events/category/Wedding flowers 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Wedding Flowers",
+        img: "/images/events/category/Wedding flowers 4.jpeg",
+    },
+];
+
+export const eventCategorDummy_15 = [
+    {
+        id: "1",
+        name: "Photography",
+        img: "/images/events/category/Photography 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Photography",
+        img: "/images/events/category/Photography 2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Photography",
+        img: "/images/events/category/Photography 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Photography",
+        img: "/images/events/category/Photography 4.jpeg",
+    },
+];
+
+export const eventCategorDummy_16 = [
+    {
+        id: "1",
+        name: "Wedding Invitations",
+        img: "/images/events/category/Wedding Invitations 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Wedding Invitations",
+        img: "/images/events/category/Wedding Invitations 2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Wedding Invitations",
+        img: "/images/events/category/Wedding Invitations 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Wedding Invitations",
+        img: "/images/events/category/Wedding Invitations 4.jpeg",
+    },
+];
+
+export const eventCategorDummy_17 = [
+    {
+        id: "1",
+        name: "Wedding Gifts (For Guests)",
+        img: "/images/events/category/Wedding Gifts 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Wedding Gifts (For Guests)",
+        img: "/images/events/category/Wedding Gifts 2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Wedding Gifts (For Guests)",
+        img: "/images/events/category/Wedding Gifts 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Wedding Gifts (For Guests)",
+        img: "/images/events/category/Wedding Gifts 4.jpeg",
+    },
+];
+
+export const eventCategorDummy_18 = [
+    {
+        id: "1",
+        name: "Wedding Halls",
+        img: "/images/events/category/Wedding Halls 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Wedding Halls",
+        img: "/images/events/category/Wedding Halls 2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Wedding Halls",
+        img: "/images/events/category/Wedding Halls 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Wedding Halls",
+        img: "/images/events/category/Wedding Halls 4.jpeg",
+    },
+];
+
+export const eventCategorDummy_19 = [
+    {
+        id: "1",
+        name: "Bridal Wears",
+        img: "/images/events/category/Bridal wears 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Bridal Wears",
+        img: "/images/events/category/Bridal wears 2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Bridal Wears",
+        img: "/images/events/category/Bridal wears 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Bridal Wears",
+        img: "/images/events/category/Bridal wears 4.jpeg",
+    },
+];
+
+export const eventCategorDummy_20 = [
+    {
+        id: "1",
+        name: "Groom Wears",
+        img: "/images/events/category/Groom wears 1.jpeg",
+    },
+    {
+        id: "2",
+        name: "Groom Wears",
+        img: "/images/events/category/Groom wears 2.jpeg",
+    },
+    {
+        id: "3",
+        name: "Groom Wears",
+        img: "/images/events/category/Groom wears 3.jpeg",
+    },
+    {
+        id: "4",
+        name: "Groom Wears",
+        img: "/images/events/category/Groom wears 4.jpeg",
+    },
+];
